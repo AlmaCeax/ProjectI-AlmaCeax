207beaf0c408eb1c56d8dc9c60f8e3a2c25fe304
Axel Alavedra Cabello <axel.alavedra@gmail.com>
2018-04-18 15:40:04 +0200

    Added ModuleFonts

--- X-Multiply_Tribute/ModuleTextures.cpp
+++ X-Multiply_Tribute/ModuleTextures.cpp
@@ -2,6 +2,7 @@
 #include "Application.h"
 #include "ModuleRender.h"
 #include "ModuleTextures.h"
+#include <string.h>
 
 #include "SDL/include/SDL.h"
 #include "SDL_image/include/SDL_image.h"
@@ -52,35 +53,33 @@ bool ModuleTextures::CleanUp()
 // Load new texture from file path
 SDL_Texture* const ModuleTextures::Load(const char* path)
 {
-	SDL_Texture* texture = NULL;
- 	SDL_Surface* surface = IMG_Load(path);
+	SDL_Texture* texture = nullptr;
 
-	if (!surface) {
-		LOG("IMG_Load: %s\n", IMG_GetError());
-		// handle error
+	// It is a new texture, let's load it !
+	SDL_Surface* surface = IMG_Load(path);
+
+	if (surface == nullptr)
+	{
+		LOG("Could not load surface with path: %s. IMG_Load: %s", path, IMG_GetError());
 	}
 	else
 	{
 		texture = SDL_CreateTextureFromSurface(App->render->renderer, surface);
 
-		if (texture == NULL)
+		if (texture == nullptr)
 		{
 			LOG("Unable to create texture from surface! SDL Error: %s\n", SDL_GetError());
 		}
 		else
 		{
-			bool room = false;
-			for (int i = 0; i < MAX_TEXTURES; ++i)
+			for (uint i = 0; i < MAX_TEXTURES; ++i)
 			{
 				if (textures[i] == nullptr)
 				{
 					textures[i] = texture;
-					room = true;
 					break;
 				}
 			}
-			if (room == false)
-				LOG("Texture buffer overflow");
 		}
 
 		SDL_FreeSurface(surface);
@@ -93,20 +92,46 @@ bool ModuleTextures::Unload(SDL_Texture * texture)
 {
 	bool ret = false;
 
-	if (texture != nullptr)
+	for (uint i = 0; i < MAX_TEXTURES; ++i)
 	{
-		for (int i = 0; i < MAX_TEXTURES; ++i)
+		if (texture == textures[i])
 		{
-			if (textures[i] == texture)
-			{
-				textures[i] = nullptr;
-				ret = true;
-				break;
-			}
+			SDL_DestroyTexture(textures[i]);
+			textures[i] = nullptr;
+			ret = true;
+			break;
 		}
-
-		SDL_DestroyTexture(texture);
 	}
 
 	return ret;
+}
+
+// Translate a surface into a texture
+SDL_Texture* ModuleTextures::LoadSurface(SDL_Surface* surface)
+{
+	SDL_Texture* texture = SDL_CreateTextureFromSurface(App->render->renderer, surface);
+
+	if (texture == NULL)
+	{
+		LOG("Unable to create texture from surface! SDL Error: %s\n", SDL_GetError());
+	}
+	else
+	{
+		for (uint i = 0; i < MAX_TEXTURES; ++i)
+		{
+			if (textures[i] == nullptr)
+			{
+				textures[i] = texture;
+				break;
+			}
+		}
+	}
+
+	return texture;
+}
+
+// Retrieve size of a texture
+void ModuleTextures::GetSize(const SDL_Texture* texture, uint& width, uint& height) const
+{
+	SDL_QueryTexture((SDL_Texture*)texture, NULL, NULL, (int*)&width, (int*)&height);
 }
